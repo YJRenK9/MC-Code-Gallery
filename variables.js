6c24a2df12d51effebd5b52bb95bfa6eaaad8671
YJRenK9 <jordan.quan.le@gmail.com>
2023-02-21 09:19:29 -1000

    Made some minor code changes

--- variables.js
+++ variables.js
@@ -1,17 +1,17 @@
-var dataType4 = "bool ";
+var dataType = "bool ";
 
-var variableName10 = "activateGlitchArt ";
-var variableName11 = "keepInventory ";
+var variableName1 = "activateGlitchArt";
+var variableName2 = "keepInventory";
 var boolValue1 = false;
 var boolValue2 = true;
 
-var paragraph4 = document.getElementById("booleans1");
-paragraph4.innerHTML = dataType4 + variableName10 + "= " + boolValue1;
-paragraph4.innerHTML += "\t\t\t\t" + dataType4 + variableName10 + "= " + boolValue2;
+var paragraph1 = document.getElementById("booleans1");
+paragraph1.innerHTML = dataType + variableName1 + " = " + boolValue1;
+paragraph1.innerHTML += "\t\t\t\t" + dataType + variableName1 + " = " + boolValue2;
 
-var paragraph5 = document.getElementById("booleans2");
-paragraph5.innerHTML = dataType4 + variableName11 + "= " + boolValue1;
-paragraph5.innerHTML += "\t\t\t\t" + dataType4 + variableName11 + "= " + boolValue2;
+var paragraph2 = document.getElementById("booleans2");
+paragraph2.innerHTML = dataType + variableName2 + " = " + boolValue1;
+paragraph2.innerHTML += "\t\t\t\t" + dataType + variableName2 + " = " + boolValue2;
 
 /*
     Unicode for whitespace: \u00A0
